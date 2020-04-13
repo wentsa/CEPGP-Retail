0837ab6b26a44216152198352c4b254b45b6e1b5
Roger Moore <Rogeramoore33@gmail.com>
2020-04-13 13:16:54 +1000

    Minor adjustments to inital GP xml
    Updated version number

--- Core.lua
+++ Core.lua
@@ -1,6 +1,6 @@
 --[[ Globals ]]--
 
-CEPGP_VERSION = "1.12.11.beta-1"
+CEPGP_VERSION = "1.12.11.release"
 SLASH_CEPGP1 = "/CEPGP";
 SLASH_CEPGP2 = "/cep";
 CEPGP_VERSION_NOTIFIED = false;
@@ -166,7 +166,7 @@ CEPGP = {
 
 CEPGP_Info = {
 	Version = "1.12.11",
-	Build = "Closed Beta 1",
+	Build = "Release",
 	NumExcluded = 0,
 	IgnoreUpdates = false,
 	LastImport = time(),
